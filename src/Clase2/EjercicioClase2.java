33a42d2e3b11f85e691aedd3bd50e03f7991df3b
Claudio Castelo <claudiocastelo@hotmail.com>
2023-03-05 14:35:18 -0300

    Subiendo primera vez...

---
+++ src/Clase2/EjercicioClase2.java
@@ -0,0 +1,10 @@
+package Clase2;
+
+public class EjercicioClase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Link repositorio: https://github.com/ccastelovcpaz/ArgentinaProgramaJava.git");
+	}
+
+}
